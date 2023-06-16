378dc409ac2c525fd3143cf6881b21264a7c2e51
wstarY25 <w_star25@naver.com>
2023-06-16 18:02:37 +0900

    Feat: 텍스트 추가 기능 구현, 색상 선택 확장

--- src/Main.js
+++ src/Main.js
@@ -6,26 +6,33 @@ import Canvas from "./components/Canvas";
 
 export default function Main() {
   const canvasRef = useRef(null);
+  const [canvasHistory, setCanvasHistory] = useState([]);
+  const [currentStateIndex, setCurrentStateIndex] = useState(0);
   const [active, setActive] = useState('cursor');
   const [cropRatio, setCropRatio] = useState(0);
   const [pencilColor, setPencilColor] = useState('black');
   const [textColor, setTextColor] = useState('black');
+  const textRef = useRef('');
 
   return (
     <Body>
       <Top>
         <TopBar canvasRef={canvasRef}
+          canvasHistory={canvasHistory} setCanvasHistory={setCanvasHistory}
+          currentStateIndex={currentStateIndex} setCurrentStateIndex={setCurrentStateIndex}
           active={active} setActive={setActive}
           cropRatio={cropRatio} setCropRatio={setCropRatio}
           pencilColor={pencilColor} setPencilColor={setPencilColor}
-          textColor={textColor} setTextColor={setTextColor} />
+          textColor={textColor} setTextColor={setTextColor} textRef={textRef} />
       </Top>
       <CanvasContainer>
         <Canvas canvasRef={canvasRef}
+          canvasHistory={canvasHistory} setCanvasHistory={setCanvasHistory}
+          currentStateIndex={currentStateIndex} setCurrentStateIndex={setCurrentStateIndex}
           active={active} setActive={setActive}
           cropRatio={cropRatio} setCropRatio={setCropRatio}
           pencilColor={pencilColor} setPencilColor={setPencilColor}
-          textColor={textColor} setTextColor={setTextColor} />
+          textColor={textColor} setTextColor={setTextColor} textRef={textRef} />
         <Name>© 모바일앱개발협동조합</Name>
       </CanvasContainer>
     </Body>
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { ChromePicker } from 'react-color';
 
 import { ReactComponent as NewPageIcon } from "../images/page.svg";
 import { ReactComponent as ImageIcon } from "../images/image.svg";
@@ -13,14 +14,16 @@ import { ReactComponent as PencilIcon } from "../images/pencil.svg";
 import { ReactComponent as UndoIcon } from "../images/undo.svg";
 import { ReactComponent as RedoIcon } from "../images/redo.svg";
 
+import { saveHistory } from "../hooks/SaveHistory";
 import { inputImage } from "../hooks/InputImage";
 import { saveFile } from "../hooks/SaveFile";
 import { rotation } from "../hooks/Rotation";
 import { filter } from "../hooks/Filter";
+import { undo, redo } from "../hooks/UndoRedo";
 
 
-export default function TopBar({ canvasRef, active, setActive,
-                                 cropRatio, setCropRatio, pencilColor, setPencilColor, textColor, setTextColor }) {
+export default function TopBar({ canvasRef, canvasHistory, setCanvasHistory, currentStateIndex, setCurrentStateIndex, active, setActive,
+                                 cropRatio, setCropRatio, pencilColor, setPencilColor, textColor, setTextColor, textRef }) {
   const [detailTopBar, setDetailTopBar] = useState('cursor');
 
   const handleClick = (props) => {
@@ -33,14 +36,35 @@ export default function TopBar({ canvasRef, active, setActive,
     if (active === 'cursor') setDetailTopBar('cursor');
   }, [active])
 
+  const handleRotation = (angle) => {
+    rotation(canvasRef, angle);
+    saveHistory(canvasRef, setCanvasHistory, currentStateIndex, setCurrentStateIndex);
+  }
+
+  const handleFilter = (effect) => {
+    filter(canvasRef, effect);
+    saveHistory(canvasRef, setCanvasHistory, currentStateIndex, setCurrentStateIndex);
+  }
+
+  const handleUndoRedo = (props) => {
+    if (props === 'undo' && currentStateIndex > 0) {
+      undo(canvasRef, canvasHistory, currentStateIndex, setCurrentStateIndex);
+    } else if (currentStateIndex < canvasHistory.length - 1) {
+      redo(canvasRef, canvasHistory, currentStateIndex, setCurrentStateIndex);
+    }
+  }
+
+  console.log(canvasHistory.length, currentStateIndex);
+
+
   return (
     <Wrapper>
       <Left>
         <NewPageIcon onClick={() => {window.location.reload()}} />
         <label htmlFor="image-upload"><ImageIcon /></label>
         <input id="image-upload" type="file" accept="image/*" style={{ display: 'none' }}
-          onChange={(e) => inputImage(e, canvasRef)} />
-        <DownloadIcon onClick={() => saveFile(canvasRef)} />
+          onChange={(e) => { inputImage(e, canvasRef); saveHistory(canvasRef, setCanvasHistory, currentStateIndex, setCurrentStateIndex); }} />
+        <DownloadIcon onClick={() => { saveFile(canvasRef); saveHistory(canvasRef, setCanvasHistory, currentStateIndex, setCurrentStateIndex); }} />
       </Left>
       <Center>
         <CursorIcon fill={active === 'cursor' ? 'black' : '#777777'} onClick={() => handleClick('cursor')} />
@@ -56,45 +80,32 @@ export default function TopBar({ canvasRef, active, setActive,
         <RotationIcon fill={active === 'rotation' ? 'black' : '#777777'} onClick={() => handleClick('rotation')} />
         { detailTopBar === 'rotation' && 
         <DetailBar>
-          <Detail onClick={() => rotation(canvasRef, -180)}>-180°</Detail>
-          <Detail onClick={() => rotation(canvasRef, -90)}>-90°</Detail>
-          <Detail onClick={() => rotation(canvasRef, 90)}>90°</Detail>
-          <Detail onClick={() => rotation(canvasRef, 180)}>180°</Detail>
+          <Detail onClick={() => handleRotation(-180)}>-180°</Detail>
+          <Detail onClick={() => handleRotation(-90)}>-90°</Detail>
+          <Detail onClick={() => handleRotation(90)}>90°</Detail>
+          <Detail onClick={() => handleRotation(180)}>180°</Detail>
         </DetailBar> }
         <EffectIcon fill={active === 'effect' ? 'black' : '#777777'}  onClick={() => handleClick('effect')} />
         { detailTopBar === 'effect' && 
         <DetailBar>
-          <Detail onClick={() => filter(canvasRef, 'blur(5px)')}>흐림</Detail>
-          <Detail onClick={() => filter(canvasRef, 'grayscale(100%)')}>회색조</Detail>
-          <Detail onClick={() => filter(canvasRef, 'sepia(100%)')}>세피아</Detail>
-          <Detail onClick={() => filter(canvasRef, 'invert(100%)')}>반전</Detail>
+          <Detail onClick={() => handleFilter('blur(5px)')}>흐림</Detail>
+          <Detail onClick={() => handleFilter('grayscale(100%)')}>회색조</Detail>
+          <Detail onClick={() => handleFilter('sepia(100%)')}>세피아</Detail>
+          <Detail onClick={() => handleFilter('invert(100%)')}>반전</Detail>
         </DetailBar> }
         <TextIcon fill={active === 'text' ? 'black' : '#777777'}  onClick={() => handleClick('text')} />
         { detailTopBar === 'text' && 
         <DetailBar>
-          <Detail><Color color='black' border={textColor === 'black' ? "true" : "false"} onClick={() => setTextColor('black')} /></Detail>
-          <Detail><Color color='white' border={textColor === 'white' ? "true" : "false"} onClick={() => setTextColor('white')} /></Detail>
-          <Detail><Color color='red' border={textColor === 'red' ? "true" : "false"} onClick={() => setTextColor('red')} /></Detail>
-          <Detail><Color color='yellow' border={textColor === 'yellow' ? "true" : "false"} onClick={() => setTextColor('yellow')} /></Detail>
-          <Detail><Color color='green' border={textColor === 'green' ? "true" : "false"} onClick={() => setTextColor('green')} /></Detail>
-          <Detail><Color color='blue' border={textColor === 'blue' ? "true" : "false"} onClick={() => setTextColor('blue')} /></Detail>
-          <Detail><Color color='purple' border={textColor === 'purple' ? "true" : "false"} onClick={() => setTextColor('purple')} /></Detail>
+          <Detail><input ref={textRef} type="text" id="text" name="text" maxLength="20" placeholder="텍스트를 입력하세요" /></Detail>
+          <TextColorPicker><ChromePicker color={textColor} onChange={(color) => setTextColor(color.hex)} /></TextColorPicker>
         </DetailBar> }
         <PencilIcon fill={active === 'pencil' ? 'black' : '#777777'}  onClick={() => handleClick('pencil')} />
         { detailTopBar === 'pencil' && 
-        <DetailBar>
-          <Detail><Color color='black' border={pencilColor === 'black' ? "true" : "false"} onClick={() => setPencilColor('black')} /></Detail>
-          <Detail><Color color='white' border={pencilColor === 'white' ? "true" : "false"} onClick={() => setPencilColor('white')} /></Detail>
-          <Detail><Color color='red' border={pencilColor === 'red' ? "true" : "false"} onClick={() => setPencilColor('red')} /></Detail>
-          <Detail><Color color='yellow' border={pencilColor === 'yellow' ? "true" : "false"} onClick={() => setPencilColor('yellow')} /></Detail>
-          <Detail><Color color='green' border={pencilColor === 'green' ? "true" : "false"} onClick={() => setPencilColor('green')} /></Detail>
-          <Detail><Color color='blue' border={pencilColor === 'blue' ? "true" : "false"} onClick={() => setPencilColor('blue')} /></Detail>
-          <Detail><Color color='purple' border={pencilColor === 'purple' ? "true" : "false"} onClick={() => setPencilColor('purple')} /></Detail>
-        </DetailBar> }
+        <PencilColorPicker><ChromePicker color={pencilColor} onChange={(color) => setPencilColor(color.hex)} /></PencilColorPicker> }
       </Center>
       <Right>
-        <UndoIcon />
-        <RedoIcon />
+        <UndoIcon onClick={() => handleUndoRedo('undo')} />
+        <RedoIcon onClick={() => handleUndoRedo('redo')} />
       </Right>
     </Wrapper>
   );
@@ -177,12 +188,27 @@ const Detail = styled.div`
   cursor: pointer;
   &:hover { font-weight: bold; }
   &:not(:last-child) { margin-right: 25px; }
+
+  input {
+    width: 150px;
+    height: 30px;
+    font-size: 16px;
+    line-height: 30px;
+    border: 0;
+    margin-left: 25px;
+  }
+  input:focus { outline: none; }
 `;
 
-const Color = styled.div`
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  border: ${(props) => props.border === "true" ? '3px solid #AAAAAA' : '1px solid #CCCCCC'};
-  background-color: ${(props) => props.color};
+const TextColorPicker = styled.div`
+  position: absolute;
+  top: 50px;
+  right: 0;
+`;
+
+const PencilColorPicker = styled.div`
+  position: absolute;
+  top: 60px;
+  right: 0;
+  z-index: 1;
 `;
